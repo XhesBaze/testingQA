ab3f41a98e6a5a4488b70a291fb7567e63f33e1c
XhesBaze <102756527+XhesBaze@users.noreply.github.com>
2023-12-22 19:51:39 +0100

    Update Bill.java

--- src/main/java/application/tryingpr/Models/Bill.java
+++ src/main/java/application/tryingpr/Models/Bill.java
@@ -1,15 +1,38 @@
 package application.tryingpr.Models;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * @param books             List of books in the bill
  * @param billId            Unique id of the bill
- * @param quantity          Quantity of books in the bill
+ * @param quantity          Quantity of books in the bill (non-negative)
  * @param totalAmount       Total amount of the bill
  * @param dateOfTransaction Date of the transaction
- */ // Model class for representing a bill
+ */
 public record Bill(ArrayList<Book> books, String billId, int quantity, double totalAmount, String dateOfTransaction) {
 
-    // Constructor to initialize the bill object with the given details
+
+    public Bill {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity cannot be zero or negative");
+        }
+        if(quantity > 500000){
+            throw new IllegalArgumentException("Quantity cannot be a number this big");
+        }
+        if(totalAmount <= 0){
+            throw new IllegalArgumentException("Total amount cannot be less than or equal to 0");
+        }
+        if(billId == null){
+            throw new IllegalArgumentException("ID cannot be null");
+        }
+        if(Objects.equals(dateOfTransaction, "")){
+            throw new IllegalArgumentException("Date of transaction cannot be empty");
+        }
+    }
+
+
+    public static void createBill(ArrayList<Book> books, String billId, int quantity, double totalAmount, String dateOfTransaction) {
+        new Bill(books, billId, quantity, totalAmount, dateOfTransaction);
+    }
 }
