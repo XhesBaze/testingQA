bf52bcbba4784050dd3ed4ed57fda432194a260d
XhesBaze <102756527+XhesBaze@users.noreply.github.com>
2024-01-09 16:28:08 +0100

    Update WritingFilesTest.java

--- src/test/MethodsForFullCoverage/WritingFilesTest.java
+++ src/test/MethodsForFullCoverage/WritingFilesTest.java
@@ -40,20 +40,18 @@ public class WritingFilesTest {
     @Test
     void testNullFilePath() {
         try {
-            // Assuming the readCredentials method is in YourClass
             writingToFiles.readCredentials("validUsername", "validPassword", null);
 
-            // If the method doesn't throw an exception, fail the test
             fail("Expected RuntimeException but no exception was thrown");
 
         } catch (RuntimeException ex) {
-            // Assert that the exception message is null
+
             assertNull(ex.getMessage());
         }
     }
     @Test
     public void testGetBooks() {
-        // Test Case 1: Valid file with correct data
+        
         File validFile = createTestFile("ValidFile.csv",
                 "Book1,Author1,20.0,4.5,300,Fiction,Publisher1,2022-01-01,10,2021-01-01\n" +
                         "Book2,Author2,15.0,3.8,250,Non-Fiction,Publisher2,2022-02-01,5,2021-02-01");
@@ -61,14 +59,13 @@ public class WritingFilesTest {
         ObservableList<Book> result1 = writingToFiles.getBooks(validFile.getAbsolutePath());
         assertEquals(2, result1.size());
 
-        // Test Case 2: File with invalid data causing NumberFormatException
         File invalidFile = createTestFile("InvalidFile.csv",
                 "Book1,Author1,20.0,4.5,300,Fiction,Publisher1,2022-01-01,10,2021-01-01\n" +
                         "Book2,Author2,15.0,InvalidRating,250,Non-Fiction,Publisher2,2022-02-01,5,2021-02-01");
 
         assertThrows(RuntimeException.class, () -> writingToFiles.getBooks(invalidFile.getAbsolutePath()));
 
-        // Note: No cleanup for files
+       
     }
     private File createTestFile(String fileName, String content) {
         try {
